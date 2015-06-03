8dd65669d97568105a02d5b18be8e9e97365c2ee
Comzyh <comzyh@gmail.com>
2015-06-03 16:37:58 +0800

    left-hand lexical

--- Tokenizer.py
+++ Tokenizer.py
@@ -3,9 +3,10 @@
 # @Author: Comzyh
 # @Date:   2015-06-01 19:05:49
 # @Last Modified by:   Comzyh
-# @Last Modified time: 2015-06-02 22:18:46
+# @Last Modified time: 2015-06-03 16:37:25
 import re
 import json
+from fa import Epsilon, NFA
 
 
 def read_lexical():
@@ -13,7 +14,7 @@ def read_lexical():
     final = []
     productions = []
     reg = re.compile(r'(?P<left>\w+)\s*=\s*((?P<epsilon>\$)|'
-                     '\"(?P<terminate>.*)\"(\s*(?P<right>\w+))?)')
+                     '((?P<right>\w+))?\s*\"(?P<terminate>.*)\")')
     for line in open('lex.txt'):
         if line[0] == '#':
             continue
@@ -29,15 +30,53 @@ def read_lexical():
             if p['terminate']:
                 p['terminate'] = json.loads('"' + p['terminate'] + '"')
             productions.append(p)
-            print p
         line_number += 1
     print 'lexical loaded, %d lines at all' % line_number
     return final, productions
 
 
+def create_nfa(final_states_name, productions):
+    alphabet = set()
+    for i in range(ord(' '), ord('~') + 1):
+        alphabet.add(chr(i))
+    nfa = NFA(alphabet)
+    name_to_state_dict = {}
+
+    def get_state_by_name(name):
+        if name not in name_to_state_dict:
+            name_to_state_dict[name] = nfa.create_node()
+        return name_to_state_dict[name]
+
+    for final_state_name in final_states_name:
+        node = nfa.create_node()
+        node.data = {'token': final_state_name}
+        nfa.final_state.add(node)
+        name_to_state_dict[final_state_name] = node
+        print 'index: %3d, name: %s' % (node.index, final_state_name)
+    for p in productions:
+        left_state = get_state_by_name(p['left'])
+        if p['epsilon']:
+            nfa.add_trans(nfa.S, Epsilon, left_state)
+            continue
+        if p['right'] is not None:
+            f = get_state_by_name(p['right'])
+        else:
+            f = nfa.S
+        for i in p['terminate'][:-1]:
+            node = nfa.create_node()
+            nfa.add_trans(f, i, node)
+            f = node
+        nfa.add_trans(f, p['terminate'][-1:], left_state)
+    nfa.name_to_state_dict = name_to_state_dict
+    return nfa
+
+
 def main():
     print 'Tokenizer by comzyh'
-    final, fproductions = read_lexical()
-
+    final, productions = read_lexical()
+    nfa = create_nfa(final, productions)
+    # print json.dumps(nfa.states)
+    # nfa.S.show()
+    # nfa.states[141].show()
 if __name__ == '__main__':
     main()
--- fa.py
+++ fa.py
@@ -3,25 +3,38 @@
 # @Author: Comzyh
 # @Date:   2015-06-02 17:38:36
 # @Last Modified by:   Comzyh
-# @Last Modified time: 2015-06-02 22:07:35
+# @Last Modified time: 2015-06-03 16:37:11
+
+Epsilon = 0
 
 
 class NFANode(object):
 
     """Node of NFA"""
 
-    def __init__(self, nfa=None, id=0, next={}, data=None):
+    def __init__(self, nfa=None, index=0, trasfer={}, data=None):
         super(NFANode, self).__init__()
-        self.id = id  # 标号
-        self.next = next  # 转移表
+        if not isinstance(index, int):
+            raise Exception('id must be integer, got %s' % index)
+        self.index = index  # 标号
+        self.trasfer = trasfer  # 转移表
         self.NFA = nfa
         self.alphabet = nfa.alphabet
         self.data = data
+        self.name_to_state_dict = None
 
-    def add_next(self, char, next):
-        if self.next[char] is None:
-            self.next[char] = []
-        self.next[char].append(next)
+    def add_trasfer(self, char, dest):
+        if char not in self.trasfer:
+            self.trasfer[char] = []
+        self.trasfer[char].append(dest)
+
+    def show(self):
+        print '--------------'
+        print 'index:%s' % self.index
+        for key, arr in self.trasfer.items():
+            print key, ':',
+            print[state.index for state in arr]
+        print '\n'
 
 
 class NFA(object):
@@ -33,18 +46,23 @@ class NFA(object):
         self.states = []  # 状态集
         self.alphabet = alphabet
         self.S = self.create_node()
-        self.finite = set()
+        self.final_state = set()
+        self.data = None
 
     def create_node(self):
-        node = NFANode(self, len(self))
+        node = NFANode(self, len(self.states))
         self.states.append(node)
         return node
 
     def add_trans(self, u, char, v):
         if (not (u in self.states) or not (v in self.states) or
-                not (char in self.alphabet)):
+                not (char in self.alphabet or char == Epsilon)):
+            print 'u in self.states: %s\n' % (u in self.states)
+            print 'v in self.states: %s\n' % (v in self.states)
+            print 'chr in self.alphabet %s\n' % (char in self.alphabet)
+            print 'char = ', char
             raise Exception('illigal transition')
-        u.add_next(char, v)
+        u.add_trasfer(char, v)
 
 
 class DFA(object):
